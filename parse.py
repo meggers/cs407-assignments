f0a885c2899db5027da7e433630cba79451ec2f9
Aarskin <mwiemer@outlook.com>
2015-03-18 20:19:24 -0500

    non breaking

--- parse.py
+++ parse.py
@@ -2,23 +2,6 @@
 #csv module.
 import csv
 
-csv_file = "RightTurnData.csv"
-
-A_X, A_Y, G_X, G_Y, T = 0, 1, 3, 4, 6
-
-CSV_DATA = [[0, 0, 0, 0, 0, 0, 0]]
-dVelocity = [[0, 0]]
-dPosition = []
-
-with open(csv_file, 'rb') as f:
-    reader = csv.reader(f)
-    reader.next()
-    for row in reader:
-        if (row[0] != ""):
-            CSV_DATA.append([float(x) for x in row])
-
-
-
 def calc_distance(accelerations):
     di = 0.0
     vi = 0.0
@@ -29,4 +12,21 @@ def calc_distance(accelerations):
         di += vi * dt
 
     return di
-    
+
+csv_file = "RightTurnData.csv"
+
+A_X, A_Y, G_X, G_Y, T = 0, 1, 3, 4, 6
+
+CSV_DATA = [[0, 0, 0, 0, 0, 0, 0]]
+x_accelerations = [[0, 0]]
+y_accelerations = [[0, 0]]
+i = 0
+
+with open(csv_file, 'rb') as f:
+    reader = csv.reader(f)
+    reader.next()
+    for row in reader:
+        if (row[0] != ""):
+            CSV_DATA.append([float(x) for x in row])
+
+
